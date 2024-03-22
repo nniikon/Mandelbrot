014ea2ac90829b47ba8325debc1823de520ee8c1
nniikon <kolya.kasparov@gmail.com>
2024-03-22 19:16:32 +0300

    add SIMD optimization

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 const int WINDOW_WIDTH  = 1920;
 const int WINDOW_HEIGHT = 1080;
 
-const int   MAX_ITERATION_DEPTH = 100;
+const int   MAX_ITERATION_DEPTH = 248;
 const float MAX_RADIUS          = 50.0;
 
-void mandelbrot_naive     (sf::Uint8* pixels, double magnifier);
+void mandelbrot_naive     (sf::Uint8* pixels, float magnifier, float shiftX);
+void mandelbrot_vectorized(sf::Uint8* pixels, float magnifier, float shiftX);
 
 int main() {
 
@@ -21,7 +22,9 @@ int main() {
 
     sf::Sprite sprite(texture);
 
-    double scale = 1.0f;
+    double scale  = 1.0f;
+    double shiftX = 0.0f;
+    double shiftY = 0.0f;
 
     while (window.isOpen())
     {
@@ -29,9 +32,28 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::RBracket)
+                {
+                    scale *= 1.1;
+                }
+                else if (event.key.code == sf::Keyboard::LBracket)
+                {
+                    scale *= 0.9;
+                }
+                else if (event.key.code == sf::Keyboard::Left)
+                {
+                    shiftX -= 0.1 / scale;
+                }
+                else if (event.key.code == sf::Keyboard::Right)
+                {
+                    shiftX += 0.1 / scale;
+                }
+            }    
         }
-    
-        mandelbrot_naive(pixels, scale);
+
+        mandelbrot_vectorized(pixels, scale, shiftX);
         texture.update(pixels);
 
         window.clear();
@@ -44,11 +66,11 @@ int main() {
     return 0;
 }
 
-void mandelbrot_naive(sf::Uint8* pixels, double magnifier)
+void mandelbrot_naive(sf::Uint8* pixels, float magnifier, float shiftX)
 {
     const float radius2 = MAX_RADIUS * MAX_RADIUS;
     const float scale = (float)WINDOW_WIDTH / WINDOW_HEIGHT;
-    const float shiftX = -0.5f;
+    shiftX -= 0.5f;
     magnifier -= 0.3;
 
     const float invMagnifier = 1.0f / magnifier;
@@ -105,3 +127,104 @@ void mandelbrot_naive(sf::Uint8* pixels, double magnifier)
 }
 
 
+void mandelbrot_vectorized(sf::Uint8* pixels, float magnifier, float shiftX)
+{
+    const float radius2 = MAX_RADIUS * MAX_RADIUS;
+    const float scale = (float)WINDOW_WIDTH / WINDOW_HEIGHT;
+    shiftX -= 0.5f;
+    magnifier -= 0.3;
+
+    const float invMagnifier = 1.0f / magnifier;
+    const float colorScale = 255.0f / MAX_ITERATION_DEPTH;
+
+    const float dx = scale * invMagnifier * (2.0f / WINDOW_WIDTH);
+    const float dy = invMagnifier * (2.0f / WINDOW_HEIGHT);
+
+    __m256 _01234567 = _mm256_set_ps(7.0f, 6.0f, 5.0f, 4.0f,
+                                     3.0f, 2.0f, 1.0f, 0.0f);
+    __m256 _8dx = _mm256_set1_ps(dx * 8);
+    __m256  _dx = _mm256_set1_ps(dx);
+    __m256  _dy = _mm256_set1_ps(dy);
+
+    __m256 _maxRadius2 = _mm256_set1_ps(radius2);
+
+    float cy = -1.0f * invMagnifier;
+    __m256 _cy = _mm256_set1_ps(cy);
+    for (int screenY = 0; screenY < WINDOW_HEIGHT; ++screenY, cy += dy)
+    {
+        float cx = shiftX - scale * invMagnifier;
+        __m256 _cx = _mm256_set1_ps(cx);
+
+        // _cx + ( 7dx, 6dx, 5dx, 4dx, 3dx, 2dx, 1dx, 0 )
+        _cx = _mm256_add_ps(_cx, _mm256_mul_ps(_dx, _01234567));
+        for (int screenX = 0; screenX < WINDOW_WIDTH; screenX += 8)
+        {
+            __m256 _x = _mm256_setzero_ps();
+            __m256 _y = _mm256_setzero_ps();
+
+            __m256 _x2 = _mm256_setzero_ps();
+            __m256 _y2 = _mm256_setzero_ps();
+            __m256 _xy = _mm256_setzero_ps();
+
+            __m256i _iterations = _mm256_setzero_si256();
+
+            for (int iteration = 0; iteration < MAX_ITERATION_DEPTH; iteration++)
+            {
+                __m256 _radius2 = _mm256_add_ps(_x2, _y2);
+
+                __m256 _cmpMask = _mm256_cmp_ps(_radius2, _maxRadius2, _CMP_LT_OQ);
+                int mask = _mm256_movemask_ps(_cmpMask);
+                // for each float:
+                //      if (radius^2 < maxRadius^2)
+                //          mask = -1
+                //      else
+                //          mask = 0
+
+                if (!mask)
+                    break;
+
+                // x = x^2 - y^2 + cx
+                // y = 2xy + cy
+                _x = _mm256_add_ps(_cx, _mm256_sub_ps(_x2, _y2));
+                _y = _mm256_add_ps(_cy, _mm256_mul_ps(_mm256_set1_ps(2.0f), _xy));
+
+                // if (radius^2 < maxRadiud^2) iteration++
+                _iterations = _mm256_sub_epi32(_iterations, _mm256_castps_si256(_cmpMask));
+
+                _x2 = _mm256_mul_ps(_x, _x);
+                _y2 = _mm256_mul_ps(_y, _y);
+                _xy = _mm256_mul_ps(_x, _y);
+            }
+
+            int* iterationsArray = (int*)&_iterations;
+            for (int i = 0; i < 8; i++)
+            {
+                sf::Uint8 r = 0;
+                sf::Uint8 g = 0;
+                sf::Uint8 b = 0;
+                if (iterationsArray[i] < MAX_ITERATION_DEPTH)
+                {
+                    float iterNormalized = iterationsArray[i] * colorScale;
+                    r = (sf::Uint8)(iterNormalized / 2);
+                    g = (sf::Uint8)(iterNormalized * 2 + 2);
+                    b = (sf::Uint8)(iterNormalized * 2 + 5);
+                }
+                else 
+                {
+                    r = 0;
+                    g = 0;
+                    b = 0;
+                }
+                int pixelIndex = (screenY * WINDOW_WIDTH + screenX + i) * 4;
+                pixels[pixelIndex + 0] = r;
+                pixels[pixelIndex + 1] = g;
+                pixels[pixelIndex + 2] = b;
+                pixels[pixelIndex + 3] = 255;
+            }
+
+
+            _cx = _mm256_add_ps(_cx, _8dx);
+        }
+        _cy = _mm256_add_ps(_cy, _dy);
+    }
+}
