04ac196b1e227b32e7ea8f3577a43ee347aa0e15
nniikon <kolya.kasparov@gmail.com>
2025-02-03 02:29:33 +0300

    optimize

--- include/mandelbrot_color.h
+++ include/mandelbrot_color.h
@@ -2,21 +2,22 @@
 #define MANDELBROT_COLOR_H_
 
 #include <SFML/Graphics.hpp>
+#include "mandelbrot_config.h"
 
-__always_inline sf::Color GetColor(int iteration, int maxIterations)
+inline __attribute__((always_inline)) sf::Color get_color(int iteration, int maxIterations)
 {
-    if (iteration >= maxIterations) {
-        return sf::Color::Black;
+    if (iteration < MAX_ITERATION_DEPTH) {
+        const float colorScale = 255.0f / MAX_ITERATION_DEPTH;
+        const float iterNormalized = iteration * colorScale;
+
+        const sf::Uint8 r = (sf::Uint8)(iterNormalized / 2 + 0);
+        const sf::Uint8 g = (sf::Uint8)(iterNormalized * 2 + 2);
+        const sf::Uint8 b = (sf::Uint8)(iterNormalized * 2 + 5);
+
+        return sf::Color(r, g, b, 255);
     }
 
-    const float colorScale = 255.0f / maxIterations;
-    const float iterNormalized = iteration * colorScale;
-
-    const sf::Uint8 r = (sf::Uint8)(iterNormalized / 2 + 0);
-    const sf::Uint8 g = (sf::Uint8)(iterNormalized * 2 + 2);
-    const sf::Uint8 b = (sf::Uint8)(iterNormalized * 2 + 5);
-
-    return sf::Color(r, g, b, 255);
+    return sf::Color::Black;
 }
 
 #endif // MANDELBROT_COLOR_H_
