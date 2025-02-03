ecfdf40222c44a193f4e7d401c3b31136fa68909
nniikon <kolya.kasparov@gmail.com>
2025-02-03 04:34:26 +0300

    improve naming

--- source/mandelbrot_arrayed.cpp
+++ source/mandelbrot_arrayed.cpp
@@ -26,69 +26,69 @@ void mandelbrot_arrayed_ranged(sf::Uint8* pixels, float magnifier, float shiftX,
     const float invMagnifier = 1.0f / magnifier;
     const float colorScale = 255.0f / MAX_ITERATION_DEPTH;
 
-    const float pixel_step_x = scale * invMagnifier * (2.0f / WINDOW_WIDTH);
-    const float pixel_step_y = invMagnifier * (2.0f / WINDOW_HEIGHT);
+    const float c_step_x = scale * invMagnifier * (2.0f / WINDOW_WIDTH);
+    const float c_step_y = invMagnifier * (2.0f / WINDOW_HEIGHT);
 
-    const float vec_pixel_step_x = pixel_step_x * VEC_SIZE;
+    const float vec_c_step_x = c_step_x * VEC_SIZE;
 
-    float current_y = -1.0f * invMagnifier + pixel_step_y * y_from;
+    float c_y = -1.0f * invMagnifier + c_step_y * y_from;
 
     for (int screenY = y_from; screenY < y_to; screenY++) 
     {
-        ALIGN float current_x[VEC_SIZE];
+        ALIGN float _c_x[VEC_SIZE];
 
-        // Initialize x coordinates
-        const float base_cx = shiftX - scale * invMagnifier;
-        FOR_VEC current_x[i] = base_cx + pixel_step_x * i;
+        const float c_x = shiftX - scale * invMagnifier;
+        FOR_VEC _c_x[i] = c_x + c_step_x * i;
 
-        // Process VEC_SIZE pixels at a time
         for (int screenX = 0; screenX < WINDOW_WIDTH; screenX += VEC_SIZE) 
         {
-            ALIGN float x [VEC_SIZE] = {0};
-            ALIGN float y [VEC_SIZE] = {0};
-            ALIGN float xy[VEC_SIZE] = {0};
-            ALIGN float x2[VEC_SIZE] = {0};
-            ALIGN float y2[VEC_SIZE] = {0};
-            ALIGN int iterations[VEC_SIZE] = {0};
+            ALIGN float _z_x [VEC_SIZE] = {0};
+            ALIGN float _z_y [VEC_SIZE] = {0};
+            ALIGN float _z_xy[VEC_SIZE] = {0};
+            ALIGN float _z_x2[VEC_SIZE] = {0};
+            ALIGN float _z_y2[VEC_SIZE] = {0};
+            ALIGN int _iterations[VEC_SIZE] = {0};
 
-            // Iteration loop
             for (int it = 0; it < MAX_ITERATION_DEPTH; ++it) 
             {
-                ALIGN float radius2[VEC_SIZE] = {0};
-                FOR_VEC radius2[i] = x2[i] + y2[i];
+                ALIGN float _radius2[VEC_SIZE] = {0};
+                FOR_VEC _radius2[i] = _z_x2[i] + _z_y2[i];
 
-                ALIGN int is_active[VEC_SIZE] = {0};
-                FOR_VEC is_active[i] = radius2[i] < MAX_RADIUS_2;
+                ALIGN int _is_active[VEC_SIZE] = {0};
+                FOR_VEC _is_active[i] = _radius2[i] < MAX_RADIUS_2;
 
                 bool all_zero = true;
-                for (int i = 0; i < VEC_SIZE; i++) {
-                    if (is_active[i] != 0) {
+                FOR_VEC
+                {
+                    if (_is_active[i] != 0)
+                    {
                         all_zero = false;
                         break;
                     }
                 }
-                if (all_zero) {
+                if (all_zero)
+                {
                     break;
                 }
 
-                FOR_VEC x[i] = x2[i] - y2[i] + current_x[i];
-                FOR_VEC y[i] = 2 * xy[i]     + current_y;
+                FOR_VEC _z_x[i] = _z_x2[i] - _z_y2[i] + _c_x[i];
+                FOR_VEC _z_y[i] = 2 * _z_xy[i]     + c_y;
 
-                FOR_VEC iterations[i] += is_active[i];
+                FOR_VEC _iterations[i] += _is_active[i];
 
-                FOR_VEC x2[i] = x[i] * x[i];
-                FOR_VEC y2[i] = y[i] * y[i];
-                FOR_VEC xy[i] = x[i] * y[i];
+                FOR_VEC _z_x2[i] = _z_x[i] * _z_x[i];
+                FOR_VEC _z_y2[i] = _z_y[i] * _z_y[i];
+                FOR_VEC _z_xy[i] = _z_x[i] * _z_y[i];
             }
 
-            for (int i = 0; i < VEC_SIZE; i++)
+            FOR_VEC
             {
                 sf::Uint8 r = 0;
                 sf::Uint8 g = 0;
                 sf::Uint8 b = 0;
-                if (iterations[i] < MAX_ITERATION_DEPTH)
+                if (_iterations[i] < MAX_ITERATION_DEPTH)
                 {
-                    float iterNormalized = iterations[i] * colorScale;
+                    float iterNormalized = _iterations[i] * colorScale;
                     r = (sf::Uint8)(iterNormalized / 2);
                     g = (sf::Uint8)(iterNormalized * 2 + 2);
                     b = (sf::Uint8)(iterNormalized * 2 + 5);
@@ -101,8 +101,8 @@ void mandelbrot_arrayed_ranged(sf::Uint8* pixels, float magnifier, float shiftX,
                 pixels[pixelIndex + 3] = 255;
             }
 
-            FOR_VEC current_x[i] += vec_pixel_step_x;
+            FOR_VEC _c_x[i] += vec_c_step_x;
         }
-        current_y += pixel_step_y;
+        c_y += c_step_y;
     }
 }
